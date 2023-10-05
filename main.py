1f6c62f2fcf5a33e9c602fed0dc37c81b16b81de
ra0sms <ra0sms@bk.ru>
2023-10-05 18:12:57 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,189 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice,QTimer
+from PyQt5.QtWidgets import QMessageBox
+
+
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("main_design.ui")
+ui.setWindowTitle("R-140 Remote Control")
+
+
+serial = QSerialPort()
+timer = QTimer()
+serial.setBaudRate(115200)
+portList = []
+ports = QSerialPortInfo().availablePorts()
+for port in ports:
+    portList.append(port.portName())
+ui.comL.addItems(portList)
+
+TIME_INTERVAL = 3000    # msec between button ON enabled
+all_out_off = "AM10000000000000000\r"
+check_out_list = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
+header = "AM1"
+is_push_on_btn = 0
+
+
+posX = 200
+posY = 100
+listX = []
+for x in range(100):
+    listX.append(x)
+listY = []
+for x in range(100):
+    listY.append(0)
+
+
+def set_output():
+    """Send to COM port data in the format AM10000000000000000\r"""
+    out_str = (header + ("".join(check_out_list[0:11])) + "0" + check_out_list[11] + "00" + check_out_list[12] + "\r")
+    serial.write(out_str.encode())
+
+
+def set_all_buttons_off():
+    ui.onB.setStyleSheet("background-color : window")
+    ui.stbB.setStyleSheet("background-color : gray window")
+    ui.fanB.setStyleSheet("background-color : gray window")
+    ui.offB.setStyleSheet("background-color : gray window")
+    ui.firstB.setStyleSheet("background-color : gray window")
+    ui.secondB.setStyleSheet("background-color : gray window")
+    ui.thirdB.setStyleSheet("background-color : gray window")
+    ui.fourthB.setStyleSheet("background-color : gray window")
+    ui.fifthB.setStyleSheet("background-color : gray window")
+    ui.sixthB.setStyleSheet("background-color : gray window")
+    ui.seventhB.setStyleSheet("background-color : gray window")
+    ui.eighthB.setStyleSheet("background-color : gray window")
+    ui.stbOnB.setStyleSheet("background-color : gray window")
+    ui.stbOffB.setStyleSheet("background-color : gray window")
+    ui.OpenB.setStyleSheet("background-color : gray window")
+    ui.startB.setStyleSheet("background-color : gray window")
+    for k in range(len(check_out_list)):
+        check_out_list[k] = '0'
+
+
+def show_warning_messagebox():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+    msg.setText("COM port is not opened!")
+    msg.setWindowTitle("Warning")
+    msg.setStandardButtons(QMessageBox.Ok)
+    retval = msg.exec_()
+
+
+def on_read():
+    if not serial.canReadLine():
+        return     # выходим если нечего читать
+    rx = serial.readLine()
+    rxs = str(rx, 'utf-8').strip()
+    data = rxs.split(',')
+
+
+def on_open():
+    if serial.isOpen():
+        serial.write(all_out_off.encode())
+        serial.waitForBytesWritten(10)
+        ui.OpenB.setText("OPEN")
+        ui.labelCOM.setText("COM port closed")
+        set_all_buttons_off()
+        ui.onB.setDisabled(True)
+        serial.close()
+    else:
+        serial.setPortName(ui.comL.currentText())
+        serial.open(QIODevice.ReadWrite)
+        ui.labelCOM.setText("COM port opened")
+        ui.OpenB.setText("CLOSE")
+        ui.onB.setDisabled(True)
+        set_all_buttons_off()
+
+
+def on_off():
+    if serial.isOpen():
+        serial.write(all_out_off.encode())
+        serial.waitForBytesWritten(10)
+        set_all_buttons_off()
+        ui.fanB.setStyleSheet("background-color : gray window")
+        ui.stbB.setStyleSheet("background-color : gray window")
+        ui.onB.setStyleSheet("background-color : gray window")
+        ui.offB.setStyleSheet("background-color : gray window")
+    else:
+        show_warning_messagebox()
+
+
+def on_fan():
+    global is_push_on_btn
+    if serial.isOpen():
+        check_out_list[8] = "1"
+        check_out_list[9] = "0"
+        check_out_list[10] = "0"
+        ui.fanB.setStyleSheet("background-color : red")
+        ui.stbB.setStyleSheet("background-color : gray window")
+        ui.onB.setStyleSheet("background-color : gray window")
+        ui.offB.setStyleSheet("background-color : gray window")
+        ui.stbB.setEnabled(True)
+        set_output()
+        is_push_on_btn = 0
+        ui.offB.setEnabled(True)
+    else:
+        show_warning_messagebox()
+
+
+def set_on_enabled():
+    ui.onB.setEnabled(True)
+    ui.fanB.setEnabled(True)
+    ui.stbB.setEnabled(True)
+
+
+def on_stb():
+    global is_push_on_btn
+    if serial.isOpen():
+        check_out_list[8] = "0"
+        check_out_list[9] = "1"
+        check_out_list[10] = "0"
+        ui.stbB.setStyleSheet("background-color : red")
+        ui.onB.setStyleSheet("background-color : gray window")
+        ui.fanB.setStyleSheet("background-color : gray window")
+        ui.offB.setStyleSheet("background-color : gray window")
+        set_output()
+        if is_push_on_btn == 0:
+            timer.singleShot(TIME_INTERVAL, set_on_enabled)
+            ui.stbB.setDisabled(True)
+            ui.offB.setDisabled(True)
+            ui.onB.setDisabled(True)
+        else:
+            ui.fanB.setEnabled(True)
+
+    else:
+        show_warning_messagebox()
+
+
+def on_on():
+    global is_push_on_btn
+    if serial.isOpen():
+        check_out_list[8] = "0"
+        check_out_list[9] = "0"
+        check_out_list[10] = "1"
+        ui.onB.setStyleSheet("background-color : red")
+        ui.stbB.setStyleSheet("background-color : gray window")
+        ui.fanB.setStyleSheet("background-color : gray window")
+        ui.offB.setStyleSheet("background-color : gray window")
+        set_output()
+        is_push_on_btn = 1
+        ui.fanB.setDisabled(True)
+    else:
+        show_warning_messagebox()
+
+
+set_output()
+serial.readyRead.connect(on_read)
+ui.onB.setDisabled(True)            # disabled button at start
+ui.stbB.setDisabled(True)           # disabled button at start
+ui.OpenB.clicked.connect(on_open)
+ui.offB.clicked.connect(on_off)
+ui.fanB.clicked.connect(on_fan)
+ui.stbB.clicked.connect(on_stb)
+ui.onB.clicked.connect(on_on)
+
+
+ui.show()
+app.exec()
